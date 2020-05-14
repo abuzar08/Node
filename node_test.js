3ff3245f68fced2fb66eae34b320d0233c3455d7
abuzar08 <31164428+abuzar08@users.noreply.github.com>
2020-05-15 00:01:30 +0530

    Added_event_tut

--- node_test.js
+++ node_test.js
@@ -3,4 +3,6 @@ console.log("Hello from Node");
 const tut = require ('./tutorial');
 console.log(tut);
 
-console.log(tut(1,1));
+console.log("Sum = ", tut.sum(1,1));
+console.log("Pi = ", tut.PI);
+console.log(new tut.SomeMathObject());
