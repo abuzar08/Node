1365e962ba445937f9298322f5302764700099d4
abuzar08 <31164428+abuzar08@users.noreply.github.com>
2020-05-15 00:33:01 +0530

    Removed_excess

--- node_test.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello from Node");
-
-const tut = require ('./tutorial');
-console.log(tut);
-
-console.log("Sum = ", tut.sum(1,1));
-console.log("Pi = ", tut.PI);
-console.log(new tut.SomeMathObject());
