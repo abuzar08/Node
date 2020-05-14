b4784b9fb1ad2ec4a78c47f0fe4b5b26f0d71aeb
abuzar08 <31164428+abuzar08@users.noreply.github.com>
2020-05-14 21:24:51 +0530

    Added-first-files

---
+++ node_test.js
@@ -0,0 +1,6 @@
+console.log("Hello from Node");
+
+const tut = require ('./tutorial');
+console.log(tut);
+
+console.log(tut(1,1));
---
+++ tutorial.js
@@ -0,0 +1,6 @@
+// const sum = (num1, num2) => num1 + num2; Currently invisible to the world
+const sum = (num1, num2) => num1 + num2;
+module.exports = sum;
+
+
+//console.log(sum(1,78.7));
