bb7f9fa80117fadde5aca632f46c478dec251890
abuzar08 <31164428+abuzar08@users.noreply.github.com>
2020-05-15 10:19:39 +0530

    playground added

---
+++ jvs/playground.js
@@ -0,0 +1,19 @@
+var a;
+var b;
+var c;
+
+a = 10;
+b = 20;
+c = "This is a ";
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("c = ", c);
+
+a = a + 10;
+b = b + 5;
+c = c + "string!";
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("c = ", c);
